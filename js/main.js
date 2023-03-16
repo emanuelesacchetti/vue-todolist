bbfa3cba96657eaee95522cc731e4a6fc8153784
emanuele sacchetti <manusacco92@gmail.com>
2023-03-16 15:24:05 +0100

    milestone 1 coompletato - creati dinamicamente i list item con i data e aggiunta la classe di line-through se done=true

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const { createApp } = Vue
 
 createApp({
-data() {
-    return {
-    message: 'Hello Vue!'
+    data() {
+            return {
+                taskList: [
+                    {
+                    text: 'fai quel che vuoi!',
+                    done: false  
+                    },
+                    {
+                    text: 'fallo quando vuoi!',
+                    done: false  
+                    },
+                    {
+                    text: 'fallo con chi vuoi!',
+                    done: false  
+                    }
+            ]
+            }
+    },
+    methods: {
+      
     }
-}
 }).mount('#app')
